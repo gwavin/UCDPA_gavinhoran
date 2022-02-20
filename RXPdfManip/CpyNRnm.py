7fb7f27683307f70a37d1a0af0926be887b43f76
gwavin <gavinhoran@gmail.com>
2022-02-20 22:25:20 +0000

    plans and copying

--- RXPdfManip/CpyNRnm.py
+++ RXPdfManip/CpyNRnm.py
@@ -1,8 +1,23 @@
 # this code will successfully rename the old file to the new file; perhaps I should make this a functions?
 # The features of the line will have to echo the thrombocalc line for now, I can do this
-
-
 import shutil
+import datetime
+
+# I made this to take a given filename, which I've imaginatively called filename, and to append the time and date to it, in the format
+# HHMMSSDDMMYY and then to place that in front of ".png".... Okay, I'm going to do something crazy.
+#
+def pdf_namer(filename):
+    t = '{0:%H%M%S%d%m%y}'.format(datetime.datetime.now())
+    a = '.pdf'
+    return (filename + t + a)
+
+def csv_time_namer(filename):
+    t = '{0:%H%M%S%d%m%y}'.format(datetime.datetime.now())
+    b = '.csv'
+    return (filename + t + b)
+
+
+
 
 original = r'C:\Users\gavin\OneDrive\Documents\GitHub\UCDPA_gavinhoran\RXPdfManip\rename\oldPdf\DBPrint Job (9).pdf'
 target = r'C:\Users\gavin\OneDrive\Documents\GitHub\UCDPA_gavinhoran\RXPdfManip\rename\NewPdf\newFileName.pdf'
