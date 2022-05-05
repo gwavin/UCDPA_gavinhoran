eb21c55098ceae37952344aa80143ab3da42e443
gwavin <gavinhoran@gmail.com>
2022-05-05 16:46:14 +0100

    Update functionFile.py

--- UCDPA_gavinhoran_Pycharm/functionFile.py
+++ UCDPA_gavinhoran_Pycharm/functionFile.py
@@ -22,13 +22,15 @@ def niceDate(year_week):
 
 
 # Below was how I made the bit above. d is an example of what a date looked like in the ECDC data; then I run
-# niceDate on it. technically, it shouldn't have any uppercase in it, I know that now.
+# niceDate on it. technically, it shouldn't have any uppercase in it, I know that now. I have an overconversational style of 
+# commenting. But I think it makes my code more readable. 
 d = "2013-W05"
 a = niceDate(d)
 r = datetime.datetime.strptime(d + '-1', "%Y-W%W-%w")
 
 
-#
+# I am not sure why I commented this out. I think I may have redefined it within the text of another .py file, and so there may have been a conflict. 
+# My programming discipline was not tight. 
 # def format_date(df, dateformat):
 #     """this will format the column containing dates'"""
 #     for row in df:
@@ -42,7 +44,7 @@ r = datetime.datetime.strptime(d + '-1', "%Y-W%W-%w")
 def png_time_namer(filename):
     t = '{0:%H%M%S%d%m%y}'.format(datetime.datetime.now())
     a = '.png'
-    return (filename + t + a)
+    return (filename + t + a) # This was not a rude joke. 
 
 
 def my_dater(year_week):
